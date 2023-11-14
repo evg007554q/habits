2896fc8705e355ea2a90b4f2055c48751adc22e7
evg007 <evg007554@yandex.ru>
2023-11-14 21:27:10 +0300

    users

--- apphabits/services.py
+++ apphabits/services.py
@@ -1,4 +1,4 @@
-
+from celery import shared_task
 from datetime import datetime, timedelta, timezone
 
 from dateutil.relativedelta import relativedelta
--- users/urls.py
+++ users/urls.py
@@ -13,6 +13,7 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/", UsersCreateAPIView.as_view(), name='register'),
     #
     # path('', LoginView.as_view(), name='login'),
     # path('logout/', LogoutView.as_view(), name='logout'),
